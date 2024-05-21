ebfe3fad113b38cccc6b598b5b6f5930e9ba294e
Semih AÇMALI <suhedasemihacmali@gmail.com>
2024-05-21 13:49:10 +0300

    Add files via upload

---
+++ Hafta8/app.js
@@ -0,0 +1,52 @@
+let sayi= 9.1;
+console.log(Math.floor(sayi));
+console.log(Math.ceil(sayi));
+console.log(Math.round(sayi));
+
+
+
+
+/*
+let sayilar=[1,56,7,4, 540,9,7,-12,-19,-54,-65,-59,2.6]
+
+let enkucuk=sayilar[0];
+
+
+
+for(let i=1; i<sayilar.length; i++){
+    if(enkucuk>sayilar[i]){
+        enkucuk=sayilar[i];
+        //console.log(enkucuk);
+    }
+}
+console.log(enkucuk);
+
+for(let j=0; j<sayilar.length; j++){
+    for(let i=j+1; i<sayilar.length;i++){
+        if(sayilar[j]>sayilar [i]){
+            let gecici=sayilar[i];
+            sayilar[i]=sayilar[j];
+            sayilar[j]=gecici;
+        }
+        //console.log("j:" + j + "\ti:" + i);
+    }
+    console.log(sayilar);
+}
+ 
+console.log(sayilar);
+
+*/
+
+console.log(Math.abs(-19));//mutlak değer
+console.log(Math.sqrt(81));// karekok
+console.log(Math.pow(2,10)); // üslü sayılar
+console.log(Math.round(Math.pow(125,1/3)));
+
+
+
+
+
+
+
+
+
